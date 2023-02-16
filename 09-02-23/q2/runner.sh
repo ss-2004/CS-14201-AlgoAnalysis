6032aeac5c6667ebbb17acf936f8784dd2f911cc
Shresth Sonkar <93792941+ss-2004@users.noreply.github.com>
2023-02-16 12:09:43 +0530

    Update runner.sh

--- 09-02-23/q2/runner.sh
+++ 09-02-23/q2/runner.sh
@@ -1,20 +1,20 @@
-#./bs avg.txt bTableAVG.txt
-#./hs avg.txt hTableAVG.txt
-#./is avg.txt iTableAVG.txt
+./bs avg.txt bTableAVG.txt
+./hs avg.txt hTableAVG.txt
+./is avg.txt iTableAVG.txt
 ./ms avg.txt mTableAVG.txt
-#./shs avg.txt shTableAVG.txt
-#./ss avg.txt sTableAVG.txt
+./shs avg.txt shTableAVG.txt
+./ss avg.txt sTableAVG.txt
 
-#./bs best.txt bTableBST.txt
-#./hs best.txt hTableBST.txt
-#./is best.txt iTableBST.txt
+./bs best.txt bTableBST.txt
+./hs best.txt hTableBST.txt
+./is best.txt iTableBST.txt
 ./ms best.txt mTableBST.txt
-#./shs best.txt shTableBST.txt
-#./ss best.txt sTableBST.txt
+./shs best.txt shTableBST.txt
+./ss best.txt sTableBST.txt
 
-#./bs wrst.txt bTableWST.txt
-#./hs wrst.txt hTableWST.txt
-#./is wrst.txt iTableWST.txt
+./bs wrst.txt bTableWST.txt
+./hs wrst.txt hTableWST.txt
+./is wrst.txt iTableWST.txt
 ./ms wrst.txt mTableWST.txt
-#./shs wrst.txt shTableWST.txt
-#./ss wrst.txt sTableWST.txt
+./shs wrst.txt shTableWST.txt
+./ss wrst.txt sTableWST.txt
